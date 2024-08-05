8ebccb91b025d4de0f52c13c497f8b3a176cf1b3
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-08-05 09:21:06 -0700

    upload

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # 100-textme.rb
-puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
+puts ARGV[0].scan(/(?<=from|to|flags):(\+?\w+|[-?[0-1]:?]+)/).join(',')
