aadec26f7becc06febbfa1a7647522be53b04b3b
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-08-13 15:03:49 -0700

    upload

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,19 +6,16 @@ If an invalid subreddit is given, the function should return 0
 """
 
 
-import requests
-
-
 def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    """Return the total number of subscribers on a given subreddit"""
-    url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "Linux:0x16.api.advanced:v1.0.0 (by /u/username)"
-    }
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+    return sub_info.json().get("data").get("subscribers")
