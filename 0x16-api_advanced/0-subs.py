c5656bc3a929bcf4e6e7d2054adb49b92eee7efa
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-08-13 15:57:13 -0700

    upload

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -22,7 +22,7 @@ def number_of_subscribers(subreddit):
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 404:
+    if response.status_code == 200:
 
         return 0
 
