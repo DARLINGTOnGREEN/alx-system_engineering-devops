11d46f02a76c4b2c4302c697ed4b4e6a4e4cefdc
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-08-13 14:54:42 -0700

    upload

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,17 +2,22 @@
 """Module for task 1"""
 
 
-def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
-    import requests
+import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
-    else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit"""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "Linux:0x16.api.advanced:v1.0.0 (by /u/username)"
+    }
+    params = {
+        "limit": 10
+    }
+
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code == 200:
+        print("None")
+        return
+    
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
