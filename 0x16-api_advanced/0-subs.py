023246c9370c6acff707a6d39d3166a5b8b665de
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-08-13 16:09:53 -0700

    upload

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
+This module contains a function to query the Reddit API and return the number of subscribers for a given subreddit.
 """
 
 import requests
@@ -10,15 +8,20 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit.
+    
+    Returns:
+        int: The number of subscribers for the subreddit, or 0 if the subreddit is invalid.
     """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
-
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "python:subreddit_subscriber_counter:v1.0 (by /u/your_username)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
     else:
         return 0
