5ea267112b76898c2af30db62926ecb577bfe054
DARLINGTOnGREEN <darlingtongreen40@gmail.com>
2024-08-13 16:02:10 -0700

    upload

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,14 +9,13 @@ If an invalid subreddit is given, the function should return 0
 import requests
 
 
-
 def number_of_subscribers(subreddit):
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     headers = {
 
-        "User-Agent": "My Reddit API Script (for learning purposes)"
+        "User-Agent": "Customs"
 
     }
 
