9a6434832a41060c3fab89849052b6c58e745c08
DARLINGTOnGREEN <darlingtongreen4@gmail.com>
2024-08-19 04:01:18 +0000

    upload

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # puppet codee that fixes 500 error on a wordpress site
 # editing the misspelled "phpp" to php
-exec { "fix-wordpress':
+exec { 'fix-wordpress':
     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
     path    => '/usr/bin/:/bin/:/usr/loca/bin/',
 }
